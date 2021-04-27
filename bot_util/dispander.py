306e09e2712223dbf2a5e5fba43a8adf6b861016
hawk_tomy <hawk201000211101@gmail.com>
2021-04-27 22:05:54 +0900

    fix dispander

--- bot_util/dispander.py
+++ bot_util/dispander.py
@@ -15,6 +15,9 @@ from . import _bot_config
 from .config import config
 
 
+__all__ = ('regex_discord_message_url','dispand')
+
+
 regex_discord_message_url = (
     'https://(ptb.|canary.)?discord(app)?.com/channels/'
     '(?P<guild>[0-9]{18})/(?P<channel>[0-9]{18})/(?P<message>[0-9]{18})'
@@ -22,19 +25,28 @@ regex_discord_message_url = (
 
 
 async def dispand(message):
-    messages = await extract_messsages(message)
+    messages = await extract_messages(message)
     for m in messages:
-        if m.content:
+        if m.content or m.attachments:
             await message.channel.send(embed=compose_embed(m))
+        if len(m.attachments) >= 2:
+            # Send the second and subsequent attachments
+            # #with embed (named 'embed') respectively:
+            for attachment in m.attachments[1:]:
+                embed = Embed()
+                embed.set_image(
+                    url=attachment.proxy_url
+                )
+                await message.channel.send(embed=embed)
         for embed in m.embeds:
             await message.channel.send(embed=embed)
 
 
-async def extract_messsages(message):
+async def extract_messages(message):
     messages = []
     for ids in re.finditer(regex_discord_message_url, message.content):
         if message.guild.id != int(ids['guild']):
-            return
+            continue
         fetched_message = await fetch_message_from_id(
             guild=message.guild,
             channel_id=int(ids['channel']),
