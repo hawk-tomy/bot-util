28c90de24cd12a362d4f4016663617c1e8cc19ee
tomy <67221751+hawk-tomy@users.noreply.github.com>
2021-05-21 08:26:09 +0900

    keep new line in util.py

--- bot_util/util.py
+++ bot_util/util.py
@@ -3,7 +3,7 @@ __all__ = ('split_line',)
 
 def split_line(string: str, num: int)-> list[str]:
     str1, str2 = string[:num], string[num:]
-    str1_split = str1.splitlines()
+    str1_split = str1.splitlines(keepends=True)
     str1, str12 = ''.join(str1_split[:-1]), str1_split[:-1]
     str2 = str12 + str2
     splited = [str1, str2]
