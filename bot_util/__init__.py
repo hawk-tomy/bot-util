610c9ed39dafa94326061667137e188e21162c55
tomy <67221751+hawk-tomy@users.noreply.github.com>
2021-05-21 08:23:34 +0900

    Update __init__.py

--- bot_util/__init__.py
+++ bot_util/__init__.py
@@ -1,11 +1,15 @@
 import logging
 
 
+from . import util
+from .util import *
+
+
 YAML_DUMP_CONFIG = {
     'encoding':'utf8',
     'allow_unicode':True,
     'default_flow_style':False
     }
 
-
+__all__ = ('YAML_DUMP_CONFIG',) + util.__all__
 logging.getLogger(__name__).addHandler(logging.NullHandler())
