94fa18009259332605ffeadc0bbeb421c9378249
hawk_tomy <hawk201000211101@gmail.com>
2021-05-22 00:01:57 +0900

    fix and merge code.

--- bot_util/__init__.py
+++ bot_util/__init__.py
@@ -5,11 +5,5 @@ from . import util
 from .util import *
 
 
-YAML_DUMP_CONFIG = {
-    'encoding':'utf8',
-    'allow_unicode':True,
-    'default_flow_style':False
-    }
-
-__all__ = ('YAML_DUMP_CONFIG',) + util.__all__
+__all__ = util.__all__
 logging.getLogger(__name__).addHandler(logging.NullHandler())
--- bot_util/util.py
+++ bot_util/util.py
@@ -1,12 +1,26 @@
-__all__ = ('split_line',)
+__all__ = ('YAML_DUMP_CONFIG','split_line')
+
+
+YAML_DUMP_CONFIG = {
+    'encoding':'utf8',
+    'allow_unicode':True,
+    'default_flow_style':False
+    }
 
 
 def split_line(string: str, num: int)-> list[str]:
+    if len(string) <= num:
+        return [string]
     str1, str2 = string[:num], string[num:]
     str1_split = str1.splitlines(keepends=True)
-    str1, str12 = ''.join(str1_split[:-1]), str1_split[:-1]
+    if len(str1_split) > 1:
+        str1, str12 = ''.join(str1_split[:-1]), str1_split[-1]
+    else:
+        str1, str12 = str1_split[0], ''
     str2 = str12 + str2
-    splited = [str1, str2]
+    splited = [str1]
     if len(str2) > num:
         splited.extend(split_line(str2, num))
+    else:
+        splited.append(str2)
     return splited
